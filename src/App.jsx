70c696a4b4a5fa48b91354657c94f53e1754583a
VitaliiHryb <vitaliigryb@ukr.net>
2022-11-28 15:15:16 +0200

    starting thinking about project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import Header from './components/header/Header.jsx';
-import Calendar from './components/calendar/Calendar.jsx';
-import Modal from './components/modal/Modal';
+import './App.scss';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+import SearchFlightsPage from './search-flight/components/search-flights-page';
 
-import './common.scss';
-const App = () => {
-  const [Now, setNow] = useState(new Date());
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setNow(new Date());
-    }, 60000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
-  const [weekStartDate, setWeekStartDate] = useState(new Date());
-
-  const handlePrevWeek = () => {
-    setWeekStartDate(prevValue => {
-      prevValue.setDate(prevValue.getDate() - 7);
-      return new Date(prevValue.getTime());
-    });
-  };
-
-  const handleNextWeek = () => {
-    setWeekStartDate(prevValue => {
-      prevValue.setDate(prevValue.getDate() + 7);
-      return new Date(prevValue.getTime());
-    });
-  };
-
-  const handleCurrentWeek = () => {
-    setWeekStartDate(() => new Date());
-  };
-
-  const [isForm, setIsForm] = useState(false);
-
-  const openFormHandler = () => {
-    setIsForm(() => true);
-  };
-
-  const closeFormHandler = () => {
-    setIsForm(() => false);
-  };
-
-  const [reRender, setReRender] = useState(false);
-
-  const renderNewData = function () {
-    setReRender(data => {
-      return data === false ? true : false;
-    });
-  };
-
+function App() {
   return (
-    <>
-      <Header
-        nextWeek={handleNextWeek}
-        prevWeek={handlePrevWeek}
-        addCurrentDay={handleCurrentWeek}
-        weekStartDate={weekStartDate}
-        openFormHandler={openFormHandler}
-      />
-      <Calendar
-        weekStartDate={weekStartDate}
-        renderNewData={renderNewData}
-        reRender={reRender}
-        isToday={
-          Now.getFullYear() === weekStartDate.getFullYear() &&
-          Now.getDate() === weekStartDate.getDate() &&
-          Now.getMonth() === weekStartDate.getMonth()
-        }
-        Now={Now}
-      />
-      {isForm ? (
-        <Modal
-          closeFormHandler={closeFormHandler}
-          renderNewData={renderNewData}
-        />
-      ) : null}
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+        <SearchFlightsPage />
+      </BrowserRouter>
+    </Provider>
   );
-};
+}
 
 export default App;
