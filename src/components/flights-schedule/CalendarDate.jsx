25214116760789e4ee145ad58f3b63c48ba759ec
VitaliiHryb <vitaliigryb@ukr.net>
2022-12-09 13:31:17 +0200

    working on calendar

--- src/components/flights-schedule/CalendarDate.jsx
+++ src/components/flights-schedule/CalendarDate.jsx
@@ -2,10 +2,11 @@ import './CalendarDate.scss';
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import * as flightsActions from '../../AirportStore/flights.actions';
+import { calendarDateSelector } from '../../AirportStore/flights.selectors';
 import moment from 'moment';
 import 'font-awesome/css/font-awesome.min.css';
 
-function CalendarDate({ fetchFlightsList }) {
+function CalendarDate({ fetchFlightsList, date }) {
   const [activeBtn, setActiveBtn] = useState({
     yesterdayActive:
       'calendar__days__handler calendar__days__handler_yesterday',
@@ -15,10 +16,9 @@ function CalendarDate({ fetchFlightsList }) {
 
   const onYesterday = () => {
     // today is not `2018-12-01`
-    fetchFlightsList(
-      moment(`2018-12-01`).subtract(1, 'days').format('DD-MM-YYYY'),
-    );
-    // console.log(fetchFlightsList);
+    // fetchFlightsList(moment().subtract(1, 'days').format('DD-MM-YYYY'));
+    fetchFlightsList(moment(`2018-11-30`).format('DD-MM-YYYY'));
+
     setActiveBtn({
       yesterdayActive:
         'calendar__days__handler calendar__days__handler_yesterday btn-active',
@@ -31,7 +31,7 @@ function CalendarDate({ fetchFlightsList }) {
   const onToday = () => {
     // today is not `2018-12-01`
     fetchFlightsList(moment(`2018-12-01`).format('DD-MM-YYYY'));
-    // console.log(fetchFlightsList);
+
     setActiveBtn({
       todayActive:
         'calendar__days__handler calendar__days__handler_today btn-active',
@@ -45,7 +45,7 @@ function CalendarDate({ fetchFlightsList }) {
   const onTomorrow = () => {
     // today is not `2018-12-01`
     fetchFlightsList(moment(`2018-12-01`).add(1, 'days').format('DD-MM-YYYY'));
-    // console.log(fetchFlightsList);
+
     setActiveBtn({
       tomorrowActive:
         'calendar__days__handler calendar__days__handler_tomorrow btn-active',
@@ -55,25 +55,32 @@ function CalendarDate({ fetchFlightsList }) {
     });
   };
 
+  // today is not `2018-12-01`
+  const calendarDate = {
+    calendarToday: moment('2018-12-01').format('DD-MM'),
+    calendarYesterday: moment('2018-12-01').subtract(1, 'days').format('DD-MM'),
+    calendarTomorrow: moment('2018-12-01').add(1, 'days').format('DD-MM'),
+  };
+
   return (
     <div className="calendar">
       <div className="calendar__days calendar__current-day">
-        <span>01/12</span>
+        <span>{date}</span>
         <div className="calendar__days__logo">
           <i className="fa-regular fa-calendar">Calendar Logo</i>
         </div>
       </div>
       <div className="calendar__days">
         <div className={activeBtn.yesterdayActive} onClick={onYesterday}>
-          <span>30/11</span>
+          <span>{calendarDate.calendarYesterday}</span>
           <div className="calendar__days__handler_weak">yesterday</div>
         </div>
         <div className={activeBtn.todayActive} onClick={onToday}>
-          <span>01/12</span>
+          <span>{calendarDate.calendarToday}</span>
           <div className="calendar__days__handler_weak">today</div>
         </div>
         <div className={activeBtn.tomorrowActive} onClick={onTomorrow}>
-          <span>02/12</span>
+          <span>{calendarDate.calendarTomorrow}</span>
           <div className="calendar__days__handler_weak">tomorrow</div>
         </div>
       </div>
@@ -81,8 +88,14 @@ function CalendarDate({ fetchFlightsList }) {
   );
 }
 
+const mapStateToProps = state => {
+  return {
+    date: calendarDateSelector(state),
+  };
+};
+
 const mapDispatchToProps = {
   fetchFlightsList: flightsActions.fetchFlightsList,
 };
 
-export default connect(null, mapDispatchToProps)(CalendarDate);
+export default connect(mapStateToProps, mapDispatchToProps)(CalendarDate);
