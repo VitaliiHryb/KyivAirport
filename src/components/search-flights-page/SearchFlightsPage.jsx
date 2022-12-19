5d7bea65761deedd85556814c3673fa2e7c3a672
VitaliiHryb <vitaliigryb@ukr.net>
2022-12-19 23:53:08 +0200

    later

--- src/components/search-flights-page/SearchFlightsPage.jsx
+++ src/components/search-flights-page/SearchFlightsPage.jsx
@@ -1,42 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import * as flightsActions from '../../AirportStore/flights.actions';
 import moment from 'moment';
 import SearchField from '../search-field/SearchField';
 import FlightsSchedule from '../flights-schedule/FlightsSchedule';
 import PropTypes from 'prop-types';
-import Spinner from 'react-bootstrap/Spinner';
 
-function SearchFlightsPage({ fetchFlightsList }) {
-  const [loading, setLoading] = useState(false);
-
+function SearchFlightsPage({ fetchFlightsList, flightsList }) {
   useEffect(() => {
     const fakeCurrentDay = moment('2018-12-01');
     fetchFlightsList(moment(fakeCurrentDay));
-
-    // let promise = new Promise(resolve => {
-    //   fetchFlightsList(moment(fakeCurrentDay));
-    //   resolve(true);
-    // });
-    // promise.then(loaded => setLoading(loaded));
   }, []);
-
-  // return (
-  //   <React.StrictMode>
-  //     {loading === false ? (
-  //       <>
-  //         <Spinner animation="border" role="status">
-  //           <span className="visually-hidden">Loading...</span>
-  //         </Spinner>
-  //       </>
-  //     ) : (
-  //       <>
-  //         <SearchField />
-  //         <FlightsSchedule />
-  //       </>
-  //     )}
-  //   </React.StrictMode>
-  // );
 
   return (
     <React.StrictMode>
